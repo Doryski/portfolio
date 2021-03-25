2562a5dafec91dcc5b5fbb5ac443db6c7a52360c
Doryski <dominatorskyyy@gmail.com>
2021-03-25 08:29:42 +0100

    cleaned some code

--- helpers/fetcher.ts
+++
@@ -1,3 +0,0 @@
-const fetcher = (url: RequestInfo, init: RequestInit) =>
-	fetch(url, init).then((res) => res.json())
-export default fetcher
--- helpers/utils.ts
+++ helpers/utils.ts
@@ -3,3 +3,50 @@ export const CONTACT_PATH = '#contact'
 export const EMAIL_REGEX = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
 export const FORMSPREE_LINK = 'https://formspree.io/xvorpadg'
 export const LANGUAGES = ['EN', 'PL']
+export const languageInit =
+	typeof window !== 'undefined' && window.navigator.language === 'pl'
+		? 'pl'
+		: 'en'
+
+export const contentInit = {
+	nav: {
+		home: '',
+		portfolio: '',
+		contact: '',
+	},
+	home: {
+		note: '',
+		specialty: '',
+		cta: {
+			portfolio: '',
+			contact: '',
+		},
+	},
+	portfolio: {
+		sectionName: '',
+		show: {
+			showSite: '',
+			showCode: '',
+		},
+		showMore: '',
+	},
+	contact: {
+		sectionName: '',
+		name: '',
+		email: '',
+		message: '',
+		errors: {
+			name: '',
+			email: '',
+			emailPattern: '',
+			message: '',
+		},
+		submit: '',
+		sending: '',
+		success: '',
+		failure: '',
+	},
+	footer: {
+		createdBy: '',
+	},
+}
