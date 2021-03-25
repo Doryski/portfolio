dc7db1c34665274b4a5508906224ffff652589e9
Doryski <dominatorskyyy@gmail.com>
2021-03-25 23:04:54 +0100

    added dumpster folder and removed showMore button

--- components/PortfolioPage/CardsWrapper.tsx
+++ components/PortfolioPage/CardsWrapper.tsx
@@ -1,22 +1,16 @@
-import React, { useContext, useState } from 'react'
-import ShowMore from './ShowMore'
+import React, { useContext } from 'react'
 import Card from './Card'
 import { Project } from '@/types'
 import { GlobalContext } from 'context'
 
 const CardsWrapper = () => {
 	const { projects } = useContext(GlobalContext)
-	const [showNumber, setShowNumber] = useState(3)
-	const showMoreProjects = () => setShowNumber((showNumber) => showNumber + 3)
 
 	return (
 		<div>
-			{projects?.slice(0, showNumber).map((project: Project) => (
+			{projects.map((project: Project) => (
 				<Card key={project.pl.name} project={project} />
 			))}
-			{showNumber <= projects?.length && (
-				<ShowMore handleClick={showMoreProjects} />
-			)}
 		</div>
 	)
 }
