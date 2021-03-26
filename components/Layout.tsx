184a6da42cd3775c30898be6fabc9665b19ad9ed
Doryski <dominatorskyyy@gmail.com>
2021-03-26 22:10:20 +0100

    modified home photo for better text visibility

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -38,7 +38,7 @@ export const BackgroundImg = styled.div`
 	position: relative;
 	width: 100%;
 	height: 100vh;
-	background-image: url('/images/bg_home.png');
+	background-image: url('/images/bg_home_.jpg');
 	background-size: cover;
 	background-position: center;
 `
