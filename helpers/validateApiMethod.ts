c71c44fc3b1ff782b09f3b5fad1cd1e6331b06e9
Doryski <dominatorskyyy@gmail.com>
2021-03-28 16:31:10 +0200

    cleaned some code

--- helpers/validateApiMethod.ts
+++ helpers/validateApiMethod.ts
@@ -1,25 +1,25 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
+type Methods = Array<'GET' | 'PUT' | 'POST' | 'DELETE'>
+
+export const statement = (x: string, y: 'is' | 'are') =>
+	`Only ${x} method${y === 'are' && 's'} ${y} available`
+
+export const reduceMethods = (methods: Methods) =>
+	methods.reduce((acc, val, index, array) => {
+		if (index === array.length - 1) return (acc += val)
+		return (acc += `${val}, `)
+	}, '')
+
 export default function validateApiMethod(
 	req: NextApiRequest,
 	res: NextApiResponse,
-	method:
-		| 'GET'
-		| 'POST'
-		| 'PUT'
-		| 'DELETE'
-		| Array<'GET' | 'PUT' | 'POST' | 'DELETE'>
+	method: 'GET' | 'POST' | 'PUT' | 'DELETE' | Methods
 ) {
-	const statement = (x: string, y: 'is' | 'are') =>
-		`Only ${x} method${y === 'are' && 's'} ${y} available`
-
 	if (typeof method === 'string' && req.method !== method)
 		return res.json(statement(method, 'is'))
 	if (method instanceof Array) {
-		const methods = method.reduce((acc, val, index, array) => {
-			if (index === array.length - 1) return (acc += val)
-			return (acc += `${val}, `)
-		}, '')
+		const methods = reduceMethods(method)
 		return res.json(statement(methods, 'are'))
 	}
 	return
